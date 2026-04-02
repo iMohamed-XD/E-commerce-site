4fd3f72db649b028c6aff75139ce0d6998f52e33
moham <mohammad.ferasamin@gmail.com>
2026-04-02 16:43:05 +0300

    email verifecation disabled

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('email_verified_at')->default(now())->nullable();
             $table->string('password')->nullable();
             $table->string('google_id')->nullable()->unique();
             $table->string('role')->default('simple_buyer');
